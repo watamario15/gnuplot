31dc2aace5ba8e4f5fb27b7f4da6d69d700d9203
Ethan A Merritt <merritt@u.washington.edu>
2024-01-03 21:46:29 -0800

    Add a mechanism for failed evaluation to return NaN rather than int_error()
    The new flag eval_fail_soft is similar to the flag string_result_only.
    If an undefined variable or function is encountered during evaluation
    the program normally calls int_error().  This prevents continuing with
    normal sequential execution of commands in a script.
    Setting eval_fail_soft = TRUE causes such a failure to instead return NaN.
    
    The only user at this point is clone_linked_axes(), and it is applied
    only to testing a named variable or function.
    This allows the "set link" command to fail soft if the linking functions
    use a variable or function that has not yet been defined.
    
    Example:
        # foo is not (yet) defined
        set link x2 via foo*x*x inv sqrt(x/foo)
    
    Bug #2677

--- src/internal.c
+++ src/internal.c
@@ -92,6 +92,7 @@ eval_reset_after_error()
     reset_stack();
     recursion_depth = 0;
     undefined = FALSE;
+    eval_fail_soft = FALSE;
 }
 
 void
@@ -101,8 +102,10 @@ f_push(union argument *x)
 
     udv = x->udv_arg;
     if (udv->udv_value.type == NOTDEFINED) {
-	if (string_result_only)
-	/* We're only here to check whether this is a string. It isn't. */
+	if (string_result_only || eval_fail_soft)
+	/* Either we're only here to check whether this is a string (it isn't)
+	 * or the caller prefers receiving NaN to a fatal error.
+	 */
 	    udv = udv_NaN;
 	else
 	    int_error(NO_CARET, "undefined variable: %s", udv->udv_name);
@@ -167,8 +170,10 @@ f_call(union argument *x)
 
     udf = x->udf_arg;
     if (!udf->at) {
-	if (string_result_only) {
-	    /* We're only here to check whether this is a string. It isn't. */
+	if (string_result_only || eval_fail_soft) {
+	    /* Either we're only here to check whether this is a string (it isn't)
+	     * or the caller prefers receiving NaN to a fatal error.
+	     */
 	    f_pop(x);
 	    push(&(udv_NaN->udv_value));
 	    return;
